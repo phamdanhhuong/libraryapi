d1f98b3521a5fafccbee3cfdb1986edd9d72757f
hieu26102004 <136700894+hieu26102004@users.noreply.github.com>
2025-01-11 21:58:02 +0700

    Thêm Api đăng nhập

--- src/main/java/com/library/libraryapi/services/UsersService.java
+++ src/main/java/com/library/libraryapi/services/UsersService.java
@@ -1,10 +1,12 @@
 package com.library.libraryapi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.library.libraryapi.dto.LoginRequest;
 import com.library.libraryapi.dto.RegisterRequest;
 import com.library.libraryapi.models.Users;
 import com.library.libraryapi.repository.UsersRepository;
@@ -37,4 +39,22 @@ public class UsersService {
 
 		return  usersRepo.save(user);
 	}
+	public Users Login(LoginRequest loginReq) {
+        // Tìm user bằng username hoặc email
+        Optional<Users> userOpt = usersRepo.findByUsername(loginReq.getUsername())
+                                           .or(() -> usersRepo.findByEmail(loginReq.getUsername()));
+
+        if (userOpt.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+
+        Users user = userOpt.get();
+
+        // Kiểm tra mật khẩu
+        if (!user.getPassword().equals(loginReq.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return user;
+    }
 }
